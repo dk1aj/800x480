212a76828b9c764d1215c6dd1bf0b702dbd4cf96
Michael <dk1aj@dg-email.de>
2025-05-17 07:31:57 +0100

    add  $fmnet to sxitch

---
+++ include/get_fmnet.php
@@ -0,0 +1,15 @@
+<?php
+function getFMNetName(string $configFilePath = '/etc/svxlink/svxlink.conf'): string
+{
+    if (!file_exists($configFilePath)) {
+        return 'UnknownNet';
+    }
+
+    $config = parse_ini_file($configFilePath, true, INI_SCANNER_RAW);
+    if (!$config || !isset($config['ReflectorLogic']['FMNET'])) {
+        return 'UnknownNet';
+    }
+
+    return trim($config['ReflectorLogic']['FMNET']);
+}
+?>
--- switch.php
+++ switch.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=800, height=480, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <meta http-equiv="refresh" content="3;url=index.php" />
- <title>SSvxDSI SWITCH</title>
+  <title>SSvxDSI SWITCH</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <style>
@@ -63,22 +63,21 @@
       background-color: #333;
     }
 
-  .feedback {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 84px;
-    padding: 16px 32px;
-    border-radius: 12px;
-    box-shadow: 0 4px 12px rgba(109, 106, 106, 0.6);
-    background-color: rgb(0, 0, 0);
-    text-align: center;
-  }
-
+    .feedback {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 84px;
+      padding: 16px 32px;
+      border-radius: 12px;
+      box-shadow: 0 4px 12px rgba(109, 106, 106, 0.6);
+      background-color: rgb(0, 0, 0);
+      text-align: center;
+    }
 
     .feedback.success {
-      color:rgb(168, 175, 76);
+      color: rgb(168, 175, 76);
     }
 
     .feedback.error {
@@ -92,73 +91,76 @@
 </head>
 <body>
 
-  <?php
-    $feedback = '';
-    $feedbackClass = 'success';
+<?php
+include_once __DIR__ . '/include/get_fmnet.php';
+$fmnet = getFMNetName(); // Gibt Wert zurück und speichert ihn in fmnet.txt
+$feedback = '';
+$feedbackClass = 'success';
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['licht_an'])) {
-            shell_exec('sudo /home/pi/pkill-pi.sh');
-            $feedback = 'Chromium reset';
-            $feedbackClass = 'error';
-        } elseif (isset($_POST['Reflector_1'])) {
-            shell_exec('sudo /home/pi/reflector1.sh');
-            $feedback = 'switch to Reflector 1';
-        } elseif (isset($_POST['Reflector_2'])) {
-            shell_exec('sudo /home/pi/reflector2.sh');
-            $feedback = 'switch to Reflector 2';
-        } elseif (isset($_POST['Reflector_3'])) {
-            shell_exec('sudo /home/pi/reflector3.sh');
-            $feedback = 'switch to Reflector 3';
-        } elseif (isset($_POST['Reflector_4'])) {
-            shell_exec('sudo /home/pi/reflector4.sh');
-            $feedback = 'switch to Reflector 4';
-        } elseif (isset($_POST['Reflector_5'])) {
-            shell_exec('sudo /home/pi/reflector5.sh');
-            $feedback = 'switch to Reflector 5';
-        }
-
-        if ($feedback !== '') {
-            echo '<div class="feedback ' . $feedbackClass . '">' . htmlspecialchars($feedback) . '</div>';
-            echo '<script>
-              window.addEventListener("DOMContentLoaded", () => {
-                document.querySelector(".grid").classList.add("hidden");
-                setTimeout(() => {
-                  window.location.href = "index.php";
-                }, 3000);
-              });
-            </script>';
-        }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['licht_an'])) {
+        shell_exec('sudo /home/pi/pkill-pi.sh');
+        $feedback = "Chromium reset [$fmnet]";
+        $feedbackClass = 'error';
+    } elseif (isset($_POST['Reflector_1'])) {
+        shell_exec('sudo /home/pi/reflector1.sh');
+        $feedback = "switch to $fmnet";
+    } elseif (isset($_POST['Reflector_2'])) {
+        shell_exec('sudo /home/pi/reflector2.sh');
+        $feedback = "switch to $fmnet";
+    } elseif (isset($_POST['Reflector_3'])) {
+        shell_exec('sudo /home/pi/reflector3.sh');
+        $feedback = "switch to $fmnet";
+    } elseif (isset($_POST['Reflector_4'])) {
+        shell_exec('sudo /home/pi/reflector4.sh');
+        $feedback = "switch to $fmnet";
+    } elseif (isset($_POST['Reflector_5'])) {
+        shell_exec('sudo /home/pi/reflector5.sh');
+        $feedback = "switch to $fmnet";
     }
-  ?>
 
-  <form method="post">
-    <main class="grid">
-      <button class="tile" type="submit" name="licht_an">
-        <i class="material-icons-outlined">sync</i>
-        Chromium reset
-      </button>
-      <button class="tile" type="submit" name="Reflector_1">
-        <i class="material-icons-outlined">settings</i>
-        Reflector 1
-      </button>
-      <button class="tile" type="submit" name="Reflector_2">
-        <i class="material-icons-outlined">settings</i>
-        Reflector 2
-      </button>
-      <button class="tile" type="submit" name="Reflector_3">
-        <i class="material-icons-outlined">settings</i>
-        Reflector 3
-      </button>
-      <button class="tile" type="submit" name="Reflector_4">
-        <i class="material-icons-outlined">settings</i>
-        Reflector 4
-      </button>
-      <button class="tile" type="submit" name="Reflector_5">
-        <i class="material-icons-outlined">sync</i>
-        Reflector 5
-      </button>
-    </main>
-  </form>
+    if ($feedback !== '') {
+        echo '<div class="feedback ' . $feedbackClass . '">' . htmlspecialchars($feedback) . '</div>';
+        echo '<script>
+          window.addEventListener("DOMContentLoaded", () => {
+            document.querySelector(".grid").classList.add("hidden");
+            setTimeout(() => {
+              window.location.href = "index.php";
+            }, 3000);
+          });
+        </script>';
+        $fmnet = ''; // Reset der Variable nach Anzeige
+    }
+}
+?>
+
+<form method="post">
+  <main class="grid">
+    <button class="tile" type="submit" name="licht_an">
+      <i class="material-icons-outlined">sync</i>
+      Chromium reset
+    </button>
+    <button class="tile" type="submit" name="Reflector_1">
+      <i class="material-icons-outlined">settings</i>
+      Reflector 1
+    </button>
+    <button class="tile" type="submit" name="Reflector_2">
+      <i class="material-icons-outlined">settings</i>
+      Reflector 2
+    </button>
+    <button class="tile" type="submit" name="Reflector_3">
+      <i class="material-icons-outlined">settings</i>
+      Reflector 3
+    </button>
+    <button class="tile" type="submit" name="Reflector_4">
+      <i class="material-icons-outlined">settings</i>
+      Reflector 4
+    </button>
+    <button class="tile" type="submit" name="Reflector_5">
+      <i class="material-icons-outlined">sync</i>
+      Reflector 5
+    </button>
+  </main>
+</form>
 </body>
 </html>
